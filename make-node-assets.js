6dca1113386c6756bff74ff15680a5d33ff50d67
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2018-05-17 17:05:16 +0200

    Added apt update on travis-ci, forgot a dot

--- make-node-assets.js
+++ make-node-assets.js
@@ -3,4 +3,4 @@ const copydir = require('copy-dir');
 const package = require('./package.json');
 
 Object.keys(package.dependencies)
-    forEach(dep => copydir.sync('./node_modules/' + dep, './node-assets/' + dep));
+    .forEach(dep => copydir.sync('./node_modules/' + dep, './node-assets/' + dep));
