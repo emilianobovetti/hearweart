0eac6e4177cc2d94eb9e6043fc7ccbda09c9e335
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2019-05-18 12:46:51 +0200

    menu order

--- make-node-assets.js
+++ make-node-assets.js
@@ -3,4 +3,4 @@ const copydir = require('copy-dir');
 const pack = require('./package.json');
 
 Object.keys(pack.dependencies)
-  .forEach(dep => copydir.sync('./node_modules/' + dep, './node-assets/' + dep));
+  .forEach(dep => copydir.sync('./node_modules/' + dep, './node-assets/' + dep, {}));
